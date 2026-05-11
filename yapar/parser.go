39ee1356cd27929bf699bbf8aabfa594a2282e87
tismajo <gir23559@uvg.edu.gt>
2026-05-10 18:18:41 -0600

    feat: extracción de tokens desde archivo yalp

--- yapar/parser.go
+++ yapar/parser.go
@@ -1,6 +1,7 @@
 package yapar
 
 import "os"
+import "strings"
 
 func ReadYalpFile(path string) (string, error) {
     data, err := os.ReadFile(path)
@@ -8,4 +9,18 @@ func ReadYalpFile(path string) (string, error) {
         return "", err
     }
     return string(data), nil
+}
+
+func ExtractTokens(content string) []string {
+    lines := strings.Split(content, "\n")
+    var tokens []string
+
+    for _, line := range lines {
+        if strings.HasPrefix(line, "%token") {
+            parts := strings.Fields(line)
+            tokens = append(tokens, parts[1:]...)
+        }
+    }
+
+    return tokens
 }
